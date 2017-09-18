0ca12125df7a757fcd4bef46453fecec59017392
rain <brother.rain.1024@gmail.com>
2017-09-18 13:41:50 +0700

    initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+================
+Underthesea Flow
+================
+
+It's a very messy project, contains a lot of stuffs useful for NLP experiments
+
---
+++ tests/test_underthesea_flow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `underthesea_flow` package."""
+
+
+import unittest
+
+from underthesea_flow import underthesea_flow
+
+
+class TestUnderthesea_flow(unittest.TestCase):
+    """Tests for `underthesea_flow` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
