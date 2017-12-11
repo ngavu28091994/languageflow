31456b72b3085c8cd5d192b92e90b2f0515a1eed
rain <brother.rain.1024@gmail.com>
2017-12-11 14:07:14 +0700

    update doc

--- languageflow/model/abc.py
+++ languageflow/model/abc.py
@@ -2,6 +2,6 @@ class ABC:
     r"""Sample doc
 
     :param Tensor data: parameter tensor
-    :param (bool, optional) requires_grad: if the parameter requires gradient. See :ref:`excluding-subgraphs` for more details.
+    :param (bool|optional) requires_grad: if the parameter requires gradient. See :ref:`excluding-subgraphs` for more details.
     """
     pass
