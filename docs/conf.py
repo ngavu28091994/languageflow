6ce79ff715af622d14aa04182f114e277531a8ee
rain <brother.rain.1024@gmail.com>
2017-12-11 15:30:57 +0700

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -281,7 +281,10 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-autodoc_mock_imports = ["sklearn"]
+autodoc_mock_imports = [
+    "sklearn",
+    "underthesea"
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
