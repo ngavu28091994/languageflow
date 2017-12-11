1e438ca3acd8edc19b1fd6c9c8da296037a06d31
rain <brother.rain.1024@gmail.com>
2017-12-11 15:41:55 +0700

    update doc

--- languageflow/model/crf.py
+++ languageflow/model/crf.py
@@ -7,9 +7,9 @@ class CRF:
         self.params = params
 
     def fit(self, X, y):
-        """Fit FastText according to X, y
+        """Fit CRF according to X, y
 
-        Parameters:
+        Parameters
         ----------
         X : list of text
             each item is a text
@@ -24,6 +24,13 @@ class CRF:
         trainer.set_params(self.params)
 
     def predict(self, X):
+        """Predict class labels for samples in X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            Samples.
+        """
         if isinstance(X[0], list):
             return [self.estimator.tag(x) for x in X]
         return self.estimator.tag(X)
