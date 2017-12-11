8a4ac1bd9e3f28032a2287392923c8d6dedc219d
rain <brother.rain.1024@gmail.com>
2017-12-11 14:00:35 +0700

    update doc

--- languageflow/model/abc.py
+++ languageflow/model/abc.py
@@ -1,4 +1,8 @@
 class ABC:
     r"""Sample doc
+    Arguments:
+        data (Tensor): parameter tensor.
+        requires_grad (bool, optional): if the parameter requires gradient. See
+            :ref:`excluding-subgraphs` for more details.
     """
     pass
