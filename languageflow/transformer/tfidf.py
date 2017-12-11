1dd76c589a445689bad62adfb6b9d28db51fa4f7
rain <brother.rain.1024@gmail.com>
2017-12-11 15:52:06 +0700

    update doc

--- languageflow/transformer/tfidf.py
+++ languageflow/transformer/tfidf.py
@@ -2,7 +2,8 @@ from sklearn import feature_extraction
 
 
 class TfidfVectorizer(feature_extraction.text.TfidfVectorizer):
-    """Convert a collection of raw documents to a matrix of TF-IDF features.
+    """
+    Convert a collection of raw documents to a matrix of TF-IDF features.
     """
     def __init__(self, *args, **kwargs):
         super(TfidfVectorizer, self).__init__(*args, **kwargs)
