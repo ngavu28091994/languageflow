b00acdde387c19ba136ad1fcefb6242427280668
rain <brother.rain.1024@gmail.com>
2017-12-11 14:17:09 +0700

    update doc

--- languageflow/model/sgd.py
+++ languageflow/model/sgd.py
@@ -4,6 +4,8 @@ from sklearn import linear_model
 
 class SGDClassifier(linear_model.SGDClassifier):
     r"""SGDClassifier
+
+    defghi hihalfsjafafs
     """
     def __init__(self, *args, **kwargs):
         super(SGDClassifier, self).__init__(*args, **kwargs)
