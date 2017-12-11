7c47b6069b2f4c16d6a72429cb083ff8b29441b1
rain <brother.rain.1024@gmail.com>
2017-12-11 15:11:10 +0700

    update doc

--- languageflow/model/sgd.py
+++ languageflow/model/sgd.py
@@ -15,20 +15,29 @@ class SGDClassifier(linear_model.SGDClassifier):
     def __init__(self, *args, **kwargs):
         super(SGDClassifier, self).__init__(*args, **kwargs)
 
-    def fit(self, *args, **kwargs):
-        """ Fit linear model with Stochastic Gradient Descent
+    def fit(self, X, y, coef_init=None, intercept_init=None,
+            sample_weight=None):
+        """Fit linear model with Stochastic Gradient Descent.
 
         Parameters
         ----------
-        arg1 : int
-            Description of arg1
-        arg2 : str
-            Description of arg2
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            Training data
+        y : numpy array, shape (n_samples,)
+            Target values
+        coef_init : array, shape (n_classes, n_features)
+            The initial coefficients to warm-start the optimization.
+        intercept_init : array, shape (n_classes,)
+            The initial intercept to warm-start the optimization.
+        sample_weight : array-like, shape (n_samples,), optional
+            Weights applied to individual samples.
+            If not provided, uniform weights are assumed. These weights will
+            be multiplied with class_weight (passed through the
+            constructor) if class_weight is specified
 
         Returns
         -------
-        int
-            Description of return value
-
+        self : returns an instance of self.
         """
-        super(SGDClassifier, self).fit(*args, **kwargs)
+        super(SGDClassifier, self).fit(X, y, coef_init, intercept_init,
+                                       sample_weight)
