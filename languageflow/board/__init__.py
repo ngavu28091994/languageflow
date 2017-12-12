1fc68d752c3dee7e08692226a3b52c326c7383e5
rain <brother.rain.1024@gmail.com>
2017-12-12 14:04:57 +0700

    add board

--- languageflow/board/__init__.py
+++ languageflow/board/__init__.py
@@ -1,9 +1,42 @@
 """
 Visualize analyzed results
 """
-
+import json
 import webbrowser
 import os
+import shutil
+from os.path import dirname, join
+
+
+def init_board(type, folder):
+    """
+
+    Parameters
+    ----------
+    type: string
+        either 'multiclass', 'multilabel'
+    folder: string
+        log folder
+    """
+    source_folder = join(dirname(__file__), type)
+    shutil.copy(join(source_folder, "index.html"), join(folder, "index.html"))
+
+
+def get_type(folder):
+    """
+
+    Parameters
+    ----------
+    folder: string
+        log folder
+
+    Returns
+    -------
+    type: string
+        type of problem
+    """
+    type = json.loads(open(join(folder, "result.json")).read())["type"]
+    return type
 
 
 def board(folder):
@@ -17,8 +50,10 @@ def board(folder):
 
     from http.server import HTTPServer, CGIHTTPRequestHandler
     port = 62000
-
+    type = get_type(folder)
+    init_board(type, folder)
     os.chdir(folder)
+
     httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
     print("Starting LanguageBoard on port: " + str(httpd.server_port))
     webbrowser.open('http://localhost:{}'.format(port))
