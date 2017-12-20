26694b7a16f453068e004244da307c1d854f7bc6
rain <brother.rain.1024@gmail.com>
2017-12-20 09:36:17 +0700

    fix doc

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ import languageflow
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.5.6'
+# needs_sphinx = '1.5.6'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
