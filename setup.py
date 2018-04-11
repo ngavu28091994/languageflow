eb2f1d73f696209d2466e5e544d872dafd07bc14
anhv <anhv.ict91@gmail.com>
2018-04-11 12:58:31 +0700

    update license

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
