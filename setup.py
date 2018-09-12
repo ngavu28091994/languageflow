b43669065ac41e4f4f07ccb05afc482480e4ec64
Vu Anh <anhv.ict91@gmail.com>
2018-09-12 16:18:16 +0700

    release version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = []
 
 setup(
     name='languageflow',
-    version='1.1.8-alpha.3',
+    version='1.1.8',
     description="Useful stuffs for NLP experiments",
     long_description=readme + '\n\n' + history,
     author="Vu Anh",
