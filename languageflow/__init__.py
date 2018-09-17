04494bc7d4e62af3787c27525c05638d5ffc1554
Vu Anh <anhv.ict91@gmail.com>
2018-09-17 08:48:48 +0700

    update version

--- languageflow/__init__.py
+++ languageflow/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Vu Anh"""
 __email__ = 'brother.rain.1024@gmail.com'
-__version__ = '1.1.6'
+__version__ = '1.1.9-alpha'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = []
 
 setup(
     name='languageflow',
-    version='1.1.8',
+    version='1.1.9-alpha',
     description="Useful stuffs for NLP experiments",
     long_description=readme + '\n\n' + history,
     author="Vu Anh",
