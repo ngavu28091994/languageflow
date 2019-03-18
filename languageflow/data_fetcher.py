7bfbbc4a23c59ce8b590c8a8c02abed54dbe4db7
Vu Anh <anhv.ict91@gmail.com>
2019-03-18 18:24:03 +0700

    GH-39: add VNTQ_SMALL, VNTQ_BIG dataset

--- languageflow/data_fetcher.py
+++ languageflow/data_fetcher.py
@@ -7,20 +7,46 @@ REPO = {
     "VNESES": {
         "cache_dir": "datasets/LTA",
         "filepath": "VNESEScorpus.txt"
-    }
+    },
+    "VNTQ_SMALL": {
+        "cache_dir": "datasets/LTA",
+        "filepath": "VNTQcorpus-small.txt"
+    },
+    "VNTQ_BIG": {
+        "cache_dir": "datasets/LTA",
+        "filepath": "VNTQcorpus-big.txt"
+    },
 }
+
+
 class DataFetcher:
     @staticmethod
     def download_data(data):
-        if data == "VNESES":
-            filepath = REPO[data]["filepath"]
-            cache_dir = REPO[data]["cache_dir"]
-            filepath = Path(CACHE_ROOT) / cache_dir / filepath
-            if Path(filepath).exists():
-                print(f"Data is already existed: '{data}' in {filepath}")
-            else:
-                url = "https://www.dropbox.com/s/m4agkrbjuvnq4el/VNESEcorpus.txt?dl=1"
-                cached_path(url, cache_dir=cache_dir)
-                shutil.move(Path(CACHE_ROOT) / cache_dir / "VNESEcorpus.txt?dl=1",
-                    Path(CACHE_ROOT) / cache_dir / filepath)
+        if data not in REPO:
+            print("No matching distribution found for '{data}'")
+            return
 
+        filepath = REPO[data]["filepath"]
+        cache_dir = REPO[data]["cache_dir"]
+        filepath = Path(CACHE_ROOT) / cache_dir / filepath
+        if Path(filepath).exists():
+            print(f"Data is already existed: '{data}' in {filepath}")
+            return
+
+        if data == "VNESES":
+            url = "https://www.dropbox.com/s/m4agkrbjuvnq4el/VNESEcorpus.txt?dl=1"
+            cached_path(url, cache_dir=cache_dir)
+            shutil.move(Path(CACHE_ROOT) / cache_dir / "VNESEcorpus.txt?dl=1",
+                        Path(CACHE_ROOT) / cache_dir / filepath)
+
+        if data == "VNTQ_SMALL":
+            url = "https://www.dropbox.com/s/b0z17fa8hm6u1rr/VNTQcorpus-small.txt?dl=1"
+            cached_path(url, cache_dir=cache_dir)
+            shutil.move(Path(CACHE_ROOT) / cache_dir / "VNTQcorpus-small.txt?dl=1",
+                        Path(CACHE_ROOT) / cache_dir / filepath)
+
+        if data == "VNTQ_BIG":
+            url = "https://www.dropbox.com/s/t4z90vs3qhpq9wg/VNTQcorpus-big.txt?dl=1"
+            cached_path(url, cache_dir=cache_dir)
+            shutil.move(Path(CACHE_ROOT) / cache_dir / "VNTQcorpus-big.txt?dl=1",
+                        Path(CACHE_ROOT) / cache_dir / filepath)
