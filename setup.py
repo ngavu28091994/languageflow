a7135ae106f4cd3ce18a124320ede7ffd25ec33d
Vu Anh <anhv.ict91@gmail.com>
2019-03-28 14:26:12 +0700

    GH-34: bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = []
 
 setup(
     name='languageflow',
-    version='1.1.9',
+    version='1.1.10-rc',
     description="Useful stuffs for NLP experiments",
     long_description=readme + '\n\n' + history,
     author="Vu Anh",
