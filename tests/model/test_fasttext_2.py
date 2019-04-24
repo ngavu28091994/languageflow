721a654040dc2820f279429e133260cfa46c4c0c
Vu Anh <anhv.ict91@gmail.com>
2019-04-24 16:03:50 +0700

    GH-63: add simple fasttext

---
+++ tests/model/test_fasttext_2.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from unittest import TestCase
+import fastText
+
+
+class TestFastText(TestCase):
+    def test_fasttext(self):
+        hyper_params = {"lr": 0.01,
+                        "epoch": 20,
+                        "wordNgrams": 3,
+                        "dim": 20}
+        model = fastText.train_supervised(input="/home/anhv/.languageflow/datasets/aivivn2019_sa/train.txt", **hyper_params)
+        dev_score = model.test("/home/anhv/.languageflow/datasets/aivivn2019_sa/dev.txt")
+        test_core = model.test("/home/anhv/.languageflow/datasets/aivivn2019_sa/test.txt")
+        print(dev_score)
+        print(test_core)
