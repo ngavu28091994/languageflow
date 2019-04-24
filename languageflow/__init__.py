bf37dec5bfe27de61aee9390794e5c52ebe6bb70
Vu Anh <anhv.ict91@gmail.com>
2019-04-24 13:06:32 +0700

    GH-58: update version file (#60)
    * GH-58: update version file

--- languageflow/__init__.py
+++ languageflow/__init__.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
-
-"""Top-level package for LanguageFlow."""
+import os
 
 __author__ = """Vu Anh"""
 __email__ = 'brother.rain.1024@gmail.com'
-__version__ = '1.1.9-alpha'
+
+###########################################################
+# Metadata
+###########################################################
+
+# Version
+try:
+    version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+    with open(version_file, 'r') as infile:
+        __version__ = infile.read().strip()
+except NameError:
+    __version__ = 'unknown (running code interactively?)'
+except IOError as ex:
+    __version__ = "unknown (%s)" % ex
